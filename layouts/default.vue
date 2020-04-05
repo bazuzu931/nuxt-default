c5621a0bed653f9a2c67426fbf41864585fa2f07
bazuzu931 <bazuzu931@yandex.ru>
2020-04-05 19:55:12 +0400

    init

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="container mx-auto flex flex-col min-h-screen mx-auto w-4/5">
+
+		<!-- Navbar -->
+		<Navbar />
+
+		<!-- Content -->
+		<main class="content flex-grow ">
+			<nuxt/>
+		</main>
+
+		<footer class="footer  ">
+			Footer
+		</footer>
+
+	</div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
+
+
+<script>
+	import Navbar from '@/components/Navbar'
+	export default
+	{
+		data: () => ({
+
+		}),
+
+		components: {
+			Navbar,
+		}
+	}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,6 @@
+<template>
+	<section>
+		<h1>Error </h1>
+		<nuxt-link to="/">Home</nuxt-link>
+	</section>
+</template>
