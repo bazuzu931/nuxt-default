a35093087640eb922c3ca2cc9791deeb6d579be0
bazuzu931 <bazuzu931@yandex.ru>
2020-04-06 02:05:45 +0400

    tailwind config

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,14 @@
 <template>
-	<div class="container mx-auto flex flex-col min-h-screen mx-auto w-4/5">
-		<h1 class="t-h1">Header Error</h1>
+	<div class="">
+		<h1 class="h h1">Header Error</h1>
+		<h1 class="h5">Header Error</h1>
+		<a href="" class="l lp">Link</a>
+		<div>
 
-		<nuxt-link to="/">Back to Home</nuxt-link>
+			<button class="btn btn-info">button</button>
+			<button class="btn  btn-pri">Blue</button>
+		</div>
+
+		<nuxt-link to="/" class="l lp bg-r-400" >Back to Home</nuxt-link>
 	</div>
 </template>
