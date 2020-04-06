0ab383c61b333816266116c5ab2ef42389f7be47
bazuzu931 <bazuzu931@yandex.ru>
2020-04-06 17:09:09 +0400

    tailwind sass

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,34 +1,9 @@
 <template>
 	<div class="">
 		<h1 class="h h1">Header Error</h1>
-		<h1 class="h5">Header Error</h1>
-		<a href="" class="l lp">Link</a>
-		<div>
 
-			<button class="btn btn-i">button</button>
-			<button class="btn  btn-p ">Blue</button>
-		</div>
 
-		<div class="grid grid-cols-6 gap-1 bg-gray-500">
-		  <div class="p-2 m-1 bg-gray-200      cs2    h-12     ">Lorem ipsum dolor.</div>
-		  <div class="p-2 m-1 bg-gray-200      cst-2 cs-3           ">Lorem ipsum dolor.</div>
-		  <div class="p-2 m-1 bg-gray-200      cst-5 cs-2           ">Lorem ipsum dolor.</div>
-		  <div class="p-2 m-1 bg-gray-200      cs-3            ">Lorem ipsum dolor.</div>
-		  <div class="p-2 m-1 bg-gray-200      cs-3             ">Lorem ipsum dolor.</div>
-		</div>
 
-		<div class="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4">
-		  <div>1</div>
-		  <div>2</div>
-		  <div>3</div>
-		  <div>4</div>
-		  <div>5</div>
-		  <div>6</div>
-		  <div>7</div>
-		  <div>8</div>
-		  <div>9</div>
-		  <div>10</div>
-		</div>
 
 	</div>
 </template>
