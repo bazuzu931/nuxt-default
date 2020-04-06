19fb7e4b68e5117a4f29f2ed682222291e6e1414
bazuzu931 <bazuzu931@yandex.ru>
2020-04-06 15:01:51 +0400

    tailwind config + DELETE OWN SPACE METADATA

--- layouts/error.vue
+++ layouts/error.vue
@@ -17,5 +17,18 @@
 		  <div class="p-2 m-1 bg-gray-200      cs-3             ">Lorem ipsum dolor.</div>
 		</div>
 
+		<div class="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4">
+		  <div>1</div>
+		  <div>2</div>
+		  <div>3</div>
+		  <div>4</div>
+		  <div>5</div>
+		  <div>6</div>
+		  <div>7</div>
+		  <div>8</div>
+		  <div>9</div>
+		  <div>10</div>
+		</div>
+
 	</div>
 </template>
