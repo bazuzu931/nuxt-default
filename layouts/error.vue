67fe1e7df7b186f89d933b6b1de8a10eb68076bd
bazuzu931 <bazuzu931@yandex.ru>
2020-04-06 12:23:51 +0400

    tailwind config

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,13 +6,20 @@
 		<div>
 
 			<button class="btn btn-info">button</button>
-			<button class="btn  btn-pri">Blue</button>
+			<button class="btn  btn-pri ">Blue</button>
 		</div>
 
-		<nuxt-link to="/" class="l lp bg-r-400" >Back to Home</nuxt-link>
-		<div class="f fr bg-gray-500">
-			<div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
-			<div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+		<div class="f  bg-gray-200 ">
+		  <div class="  fsh text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+		    Will grow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas ratione repellat, ut aliquid voluptas earum quae, nemo unde, nostrum libero officiis eaque quasi. Officiis id similique eligendi suscipit consequatur tempora corrupti, molestiae, repudiandae ipsam aliquam sint dolore deserunt, neque, eum!
+		  </div>
+		  <div class="  fsh text-gray-800 text-center bg-gray-500 px-4 py-2 m-2">
+		    Will not grow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam cum expedita dignissimos quos reprehenderit culpa? Facilis saepe odit eaque dicta doloremque magni modi? Similique commodi quos qui, perferendis aliquam rerum ducimus, libero obcaecati nostrum harum, alias nemo, ipsam laborum iure!
+		  </div>
+		  <div class="  fsh text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+		    Will grow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque temporibus non quam veritatis tempora quas nostrum officiis vero atque deserunt ipsum esse aperiam, possimus illo modi qui aliquam obcaecati suscipit dolor a maxime? Voluptatem consequuntur, voluptas debitis eveniet enim quia.
+		  </div>
 		</div>
+
 	</div>
 </template>
