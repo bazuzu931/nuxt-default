2400de5bf508510ec330e9ab77df41092127155d
bazuzu931 <bazuzu931@yandex.ru>
2020-04-06 14:07:35 +0400

    tailwind config

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,20 +5,16 @@
 		<a href="" class="l lp">Link</a>
 		<div>
 
-			<button class="btn btn-info">button</button>
-			<button class="btn  btn-pri ">Blue</button>
+			<button class="btn btn-i">button</button>
+			<button class="btn  btn-p ">Blue</button>
 		</div>
 
-		<div class="f  bg-gray-200 ">
-		  <div class="  fsh text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-		    Will grow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas ratione repellat, ut aliquid voluptas earum quae, nemo unde, nostrum libero officiis eaque quasi. Officiis id similique eligendi suscipit consequatur tempora corrupti, molestiae, repudiandae ipsam aliquam sint dolore deserunt, neque, eum!
-		  </div>
-		  <div class="  fsh text-gray-800 text-center bg-gray-500 px-4 py-2 m-2">
-		    Will not grow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam cum expedita dignissimos quos reprehenderit culpa? Facilis saepe odit eaque dicta doloremque magni modi? Similique commodi quos qui, perferendis aliquam rerum ducimus, libero obcaecati nostrum harum, alias nemo, ipsam laborum iure!
-		  </div>
-		  <div class="  fsh text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-		    Will grow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque temporibus non quam veritatis tempora quas nostrum officiis vero atque deserunt ipsum esse aperiam, possimus illo modi qui aliquam obcaecati suscipit dolor a maxime? Voluptatem consequuntur, voluptas debitis eveniet enim quia.
-		  </div>
+		<div class="grid grid-cols-6 gap-1 bg-gray-500">
+		  <div class="p-2 m-1 bg-gray-200      cs2    h-12     ">Lorem ipsum dolor.</div>
+		  <div class="p-2 m-1 bg-gray-200      cst-2 cs-3           ">Lorem ipsum dolor.</div>
+		  <div class="p-2 m-1 bg-gray-200      cst-5 cs-2           ">Lorem ipsum dolor.</div>
+		  <div class="p-2 m-1 bg-gray-200      cs-3            ">Lorem ipsum dolor.</div>
+		  <div class="p-2 m-1 bg-gray-200      cs-3             ">Lorem ipsum dolor.</div>
 		</div>
 
 	</div>
