e038a4f76a2277848a49f99f747b1a29aa8f9fc5
bazuzu931 <bazuzu931@yandex.ru>
2020-04-08 20:30:07 +0400

    init

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,16 +9,16 @@
 		</div>
 
 		<!-- Responsive navbar icon -->
-		<label for="menu-toggle" class="cursor-pointer lg:hidden block"><svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+		<label for="menu-toggle" class="cursor-pointer lg:hidden block"><svg class="fill-current text-white  md:hover:text-gray-300" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
 		<input class="hidden" type="checkbox" id="menu-toggle" />
 
 		<!-- Menu items -->
 		<div class="hidden lg:flex lg:items-center lg:w-auto w-full transition-display duration-150 ease-in-out" id="menu">
 			<nav>
 			  <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-				<li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white text-white hover:text-gray-200 font-bold transition duration-300 ease-in-out " to="/">Home</nuxt-link></li>
-				<li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white text-white hover:text-gray-200 font-bold transition duration-300 ease-in-out " to="/about">About</nuxt-link></li>
-				<li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-white text-white hover:text-gray-200 font-bold transition duration-300 ease-in-out " to="/users">Users</nuxt-link></li>
+				<li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-white text-white md:hover:text-gray-300 font-bold lg:transition duration-300 ease-in-out " to="/">Home</nuxt-link></li>
+				<li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-white text-white md:hover:text-gray-300 font-bold lg:transition duration-300 ease-in-out " to="/about">About</nuxt-link></li>
+				<li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-white text-white md:hover:text-gray-300 font-bold lg:transition duration-300 ease-in-out " to="/users">Users</nuxt-link></li>
 				<!-- <li><nuxt-link class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" to="#">Support</nuxt-link></li> -->
 			  </ul>
 			</nav>
