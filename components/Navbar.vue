6a82b8448436a06f16f94d77d7eb7924ca7be0ca
bazuzu931 <bazuzu931@yandex.ru>
2020-04-08 20:35:04 +0400

    init

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,7 +9,7 @@
 		</div>
 
 		<!-- Responsive navbar icon -->
-		<label for="menu-toggle" class="cursor-pointer lg:hidden block"><svg class="fill-current text-white  md:hover:text-gray-300" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+		<label for="menu-toggle" class="cursor-pointer lg:hidden block "><svg class="fill-current text-white  md:hover:text-gray-300" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
 		<input class="hidden" type="checkbox" id="menu-toggle" />
 
 		<!-- Menu items -->
