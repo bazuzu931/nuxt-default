1d166442a3ddb8b1d748a4e009aec6f412722a65
bazuzu931 <bazuzu931@yandex.ru>
2020-04-16 19:16:11 +0400

    add icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,18 @@
 <template>
-	<div class=" bg-orange-600 ">
+	<div class=" bg-orange-500 ">
 		<div class=" flex flex-col w-11/12 mx-auto  antialiased  py-4  ">
-			<p class="  uppercase text-gray-300 font-bold pb-4 "  >Контакты</p>
+
+
+			<div class="  ">
+
+				<ion-icon class=" text-gray-800 text-5xl "  name="headset"></ion-icon>
+
+			</div>
+
+
+			<p class="  uppercase text-gray-800 font-bold pb-4 "  >Контакты</p>
 			<div class="  pl-8 ">
-				<div class=" w-3/5 border-t border-dashed text-gray-300 tracking-wide  ">
+				<div class=" w-3/5 border-t border-dashed text-gray-800 tracking-wide  ">
 					<p class=" pb-1 "  >+7 (994) 50 111-11-11</p>
 					<p class=" pb-1 "  >+7 (994) 50 111-11-11</p>
 				</div>
@@ -17,9 +26,16 @@
 </template>
 
 <script>
-	export default {
-		data: () => ({
-
-		}),
-	}
+  import { fas } from '@fortawesome/free-solid-svg-icons'
+  import { faGithub } from '@fortawesome/free-brands-svg-icons'
+  export default {
+    computed: {
+      fas () {
+         return fas
+      },
+      faGithub () {
+         return faGithub
+      }
+    },
+  }
 </script>
