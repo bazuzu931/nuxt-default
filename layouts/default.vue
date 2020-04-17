c9f007f3bf76e34d0c2db0a3b94cc841f7599dc4
bazuzu931 <bazuzu931@yandex.ru>
2020-04-17 13:38:18 +0400

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,8 +19,8 @@
 </template>
 
 <script>
-	import Navbar from '~/components/Navbar.vue'
-	import Footer from '~/components/Footer.vue'
+	import Navbar from '@/components/Navbar'
+	import Footer from '@/components/Footer'
 	export default
 	{
 		data: () => ({
